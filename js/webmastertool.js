ecbfad8b41e69aefe7db9b2780c632754dd3c6d0
yuxi <me@yuxifan.com>
2018-04-19 17:24:49 +0200

    update

---
+++ js/webmastertool.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// on document ready
+document.addEventListener('DOMContentLoaded', function() {
+    // init tabs
+    $(function() {
+        $( "#tabs" ).tabs();
+    });
+}, false);
