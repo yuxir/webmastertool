94498926eff88ce410118845c09ecbfebf744d75
yuxi <me@yuxifan.com>
2018-04-23 12:16:18 +0200

    use fixed data to test the Vultr account UI

--- js/webmastertool.js
+++ js/webmastertool.js
@@ -5,5 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
     // init tabs
     $(function() {
         $( "#tabs" ).tabs();
+        $( "#vultr-div" ).accordion();
+
+        // Load Vultr account information
+        let balance             = '-43.01';
+        let pending_charges     = '3.72';
+        let last_payment_date   = '2018-01-22 07:52:58';
+        let last_payment_amount = '-100.00';
+        let account_html_block = '';
+        account_html_block += 'Balance: '              + balance + '<br/>';
+        account_html_block += 'Pending charges: '      + pending_charges + '<br/>';
+        account_html_block += 'Last payment date: '    + last_payment_date + '<br/>';
+        account_html_block += 'Last payment account: ' + last_payment_amount + '<br/>';
+        $("#vultr-account").html(account_html_block);
+
+
+        $("#info-window").html(' Loaded.');
     });
+
 }, false);
+
