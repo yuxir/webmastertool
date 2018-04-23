51751ac2ea3ad5e3edb2c93ea0006b309bee16e1
yuxi <me@yuxifan.com>
2018-04-23 15:34:17 +0200

    add update_account_info method

--- js/vultr.js
+++ js/vultr.js
@@ -2,28 +2,38 @@
 
 const api_url = 'https://api.vultr.com/';
 
-const update_account_info = (api_key, div) => {
-    $.ajax({
-        type: "GET",
-        //headers: {"API-Key": api_key},
-        url: 'https://api.vultr.com/v1/account/info',
-        beforeSend: function(xhr){xhr.setRequestHeader('API-Key', api_key);},
-        success: function(result) {
-            console.log(result);
-            console.log(result['balance']);
-            console.log(result['pending_charges']);
-            console.log(result['last_payment_amount']);
-            console.log(result['pending_charges']);
-        },
-        error: function(request, status, error) {
-            console.log('error');
-            console.log(request.responseText);
-            console.log(error);
-            console.log(status);
-        },
-        complete: function(data) {
-            console.log('done');
-        }
-    });
+const update_account_info = (api_key, div_id) => {
+    if (api_key) {
+        // Load Vultr account information
+        $.ajax({
+            type: "GET",
+            url: 'https://api.vultr.com/v1/account/info',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('API-Key', api_key);
+            },
+            success: function (result) {
+                let balance = result['balance'];
+                let pending_charges = result['pending_charges'];
+                let last_payment_date = result['last_payment_date'];
+                let last_payment_amount = result['last_payment_amount'];
+                let account_html_block = '';
+                account_html_block += 'Balance: ' + balance + '<br/>';
+                account_html_block += 'Pending charges: ' + pending_charges + '<br/>';
+                account_html_block += 'Last payment date: ' + last_payment_date + '<br/>';
+                account_html_block += 'Last payment account: ' + last_payment_amount + '<br/>';
+                $("#" + div_id).html(account_html_block);
+            },
+            error: function (request, status, error) {
+                let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
+                updateStatus(html);
+            },
+            complete: function (data) {
+                updateStatus('Loaded account information.');
+            }
+        });
+    }else{
+        let html = '<span style="color:red;">Invalid API key.</span>';
+        updateStatus(html);
+    }
 }
 
