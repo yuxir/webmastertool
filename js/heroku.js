f42e488465da5818fff92ebd57a39416e2d7272e
yuxi <me@yuxifan.com>
2018-05-14 23:45:02 +0100

    load dynos info

--- js/heroku.js
+++ js/heroku.js
@@ -5,7 +5,6 @@ const heroku_api_url = 'https://api.heroku.com/';
 // Load Heroku account information
 const update_heroku_account_info = (api_key, div_id) => {
     if (api_key) {
-        
         let options = {
             method: 'GET',
             headers: {
@@ -35,7 +34,7 @@ const update_heroku_account_info = (api_key, div_id) => {
             // Update status bar
             updateStatus('Loaded account info.');
         }).catch(function(err) {
-            let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
+            let html = '<span style="color:red;">Error: ' + err + '</span>';
             updateStatus(html);
         });        
     }else{
@@ -44,79 +43,76 @@ const update_heroku_account_info = (api_key, div_id) => {
     }
 }
 
-const update_heroku_apps_info = (api_key, div_id) => {
+// Update UI with apps and dynos info
+const update_heroku_apps_info = (api_key, apps_div_id, dynos_div_id) => {
     if (api_key) {
-        // Load Heroku application information
-        $.ajax({
-            type: "GET",
-            url: heroku_api_url + 'apps',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', "Bearer " + api_key);
-                xhr.setRequestHeader('Accept', "application/vnd.heroku+json; version=3");
-            },
-            success: function (result) {  // update apps info
-                let apps_html_block = '';
-                for(var s in result) {
-		           apps_html_block += '<div class="row divblock">';
-                   apps_html_block += '<div class="col-sm-4">Name</div><div class="col-sm-8">' + result[s]['name'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Buildpack</div><div class="col-sm-8">' + result[s]['buildpack_provided_description'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Repo size</div><div class="col-sm-8">' + result[s]['repo_size'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Slug size</div><div class="col-sm-8">' + result[s]['slug_size'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Created at</div><div class="col-sm-8">' + result[s]['created_at'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Released at</div><div class="col-sm-8">' + result[s]['released_at'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Updated at</div><div class="col-sm-8">' + result[s]['updated_at'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Region</div><div class="col-sm-8">' + result[s]['region']['name'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Git url</div><div class="col-sm-8">' + result[s]['git_url'] + '</div>';
-                   apps_html_block += '<div class="col-sm-4">Url</div><div class="col-sm-8"><a target="_blank" href="' + result[s]['web_url'] + '">' + result[s]['web_url'] + '</a></div>';
-   		           apps_html_block += '</div>';
-                   
-                   // update dyno info
-                   update_heroku_dynos_info(api_key, result[s]['id'], 'heroku-dynos');
-                }
-                $("#" + div_id).html(apps_html_block);
-                updateStatus('Loaded apps info.');
-            },
-            error: function (request, status, error) {
-                let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
-                updateStatus(html);
-            },
-            complete: function (data) {
+        // HTTP request headers
+        let options = {
+            method: 'GET',
+            headers: {
+              'Authorization': 'Bearer ' + api_key,
+              'Accept': 'application/vnd.heroku+json; version=3'
             }
-        });
-    }else{
-        let html = '<span style="color:red;">Invalid API key.</span>';
-        updateStatus(html);
-    }
-}
+        };
 
-const update_heroku_dynos_info = (api_key, appid, div_id) => {
-    if (api_key) {
-        // Load Heroku dyno information
-        $.ajax({
-            type: "GET",
-            url: heroku_api_url + appid + '/dynos',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', "Bearer " + api_key);
-                xhr.setRequestHeader('Accept', "application/vnd.heroku+json; version=3");
-            },
-            success: function (result) {  // update dyno info
-                let html_block = '';
-                for(var s in result) {
-		           html_block += '<div class="row divblock">';
-                   html_block += '<div class="col-sm-4">Name</div><div class="col-sm-8">' + result[s]['name'] + '</div>';
-                   html_block += '<div class="col-sm-4">ID</div><div class="col-sm-8">' + result[s]['id'] + '</div>';
-                   html_block += '</div>';
-                }
-                $("#" + div_id).html(html_block);
-                updateStatus('Loaded dyno info.');
-            },
-            error: function (request, status, error) {
-                let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
-                updateStatus(html);
-            },
-            complete: function (data) {
+        // Load Heroku application information
+        fetch(heroku_api_url + 'apps', options).then(function(response) {
+          return response.json();
+        }).then(function(data) {
+            let apps_html_block = '';
+            let app_ids = [];
+            
+            for(let s in data) {  
+                apps_html_block += '<div class="row divblock">';
+                
+                app_ids.push(data[s]['id']);                
+                apps_html_block += '<div class="col-sm-4">Name</div><div class="col-sm-8">' + data[s]['name'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Buildpack</div><div class="col-sm-8">' + data[s]['buildpack_provided_description'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Repo size</div><div class="col-sm-8">' + data[s]['repo_size'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Slug size</div><div class="col-sm-8">' + data[s]['slug_size'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Created at</div><div class="col-sm-8">' + data[s]['created_at'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Released at</div><div class="col-sm-8">' + data[s]['released_at'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Updated at</div><div class="col-sm-8">' + data[s]['updated_at'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Region</div><div class="col-sm-8">' + data[s]['region']['name'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Git url</div><div class="col-sm-8">' + data[s]['git_url'] + '</div>';
+                apps_html_block += '<div class="col-sm-4">Url</div><div class="col-sm-8"><a target="_blank" href="' + data[s]['web_url'] + '">' + data[s]['web_url'] + '</a></div>';
+   		        apps_html_block += '</div>';
+            }    
+            // Update account UI
+            $("#" + apps_div_id).html(apps_html_block);
+            // Update status bar
+            updateStatus('Loaded apps info.');
+            return app_ids;
+        }).then(function(app_ids) {
+            for(let i in app_ids){
+                // Load Heroku dynos information
+                fetch(heroku_api_url + 'apps/' + app_ids[i] + '/dynos', options).then(function(response) {
+                    return response.json();
+                }).then(function(dynos_data) {
+                    let html_block = '';
+                    for(let s in dynos_data) {
+		                html_block += '<div class="row divblock">';
+                        html_block += '<div class="col-sm-4">Name</div><div class="col-sm-8">' + dynos_data[s]['name'] + '</div>';
+                        html_block += '<div class="col-sm-4">ID</div><div class="col-sm-8">' + dynos_data[s]['id'] + '</div>';
+                        html_block += '<div class="col-sm-4">Type</div><div class="col-sm-8">' + dynos_data[s]['type'] + '</div>';
+                        html_block += '<div class="col-sm-4">Size</div><div class="col-sm-8">' + dynos_data[s]['size'] + '</div>';
+                        html_block += '<div class="col-sm-4">State</div><div class="col-sm-8">' + dynos_data[s]['state'] + '</div>';
+                        html_block += '<div class="col-sm-4">Created at</div><div class="col-sm-8">' + dynos_data[s]['created_at'] + '</div>';
+                        html_block += '<div class="col-sm-4">Updated at</div><div class="col-sm-8">' + dynos_data[s]['updated_at'] + '</div>';
+                        html_block += '<div class="col-sm-4">App name</div><div class="col-sm-8">' + dynos_data[s]['app']['name'] + '</div>';
+                        html_block += '</div>';
+                    }
+                    $("#" + dynos_div_id).html($("#" + dynos_div_id).html()+html_block);
+                    updateStatus('Loaded dyno info.');             
+                }).catch(function(err) {
+                    let html = '<span style="color:red;">Error: ' + err + '</span>';
+                    updateStatus(html);
+                });
             }
-        });
+        }).catch(function(err) {
+            let html = '<span style="color:red;">Error: ' + err + '</span>';
+            updateStatus(html);
+        });        
     }else{
         let html = '<span style="color:red;">Invalid API key.</span>';
         updateStatus(html);
