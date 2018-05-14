5f82fea2e7716af2d3f1c563ac7b754c3a09feba
yuxi <me@yuxifan.com>
2018-05-14 20:39:00 +0100

    refactor update_heroku_account_info to use fetch

--- js/heroku.js
+++ js/heroku.js
@@ -2,40 +2,42 @@
 
 const heroku_api_url = 'https://api.heroku.com/';
 
+// Load Heroku account information
 const update_heroku_account_info = (api_key, div_id) => {
     if (api_key) {
-        // Load Heroku account information
-        $.ajax({
-            type: "GET",
-            url: heroku_api_url + 'account',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', "Bearer " + api_key);
-                xhr.setRequestHeader('Accept', "application/vnd.heroku+json; version=3");
-            },
-            success: function (result) {  // update account info
-                let account_html_block = '';
-                
-		        account_html_block += '<div class="row">';
-                account_html_block += '<div class="col-sm-4">ID</div><div class="col-sm-8">' + result['id'] + '</div>';
-                account_html_block += '<div class="col-sm-4">Name</div><div class="col-sm-8">' + result['name'] + '</div>';
-                account_html_block += '<div class="col-sm-4">Email</div><div class="col-sm-8">' + result['email'] + '</div>';
-                account_html_block += '<div class="col-sm-4">Sms number</div><div class="col-sm-8">' + result['sms_number'] + '</div>';
-                account_html_block += '<div class="col-sm-4">Identity Provider</div><div class="col-sm-8">' + result['identity_provider'] + '</div>';
-                account_html_block += '<div class="col-sm-4">Created at</div><div class="col-sm-8">' + result['created_at'] + '</div>';
-                account_html_block += '<div class="col-sm-4">Updated at</div><div class="col-sm-8">' + result['updated_at'] + '</div>';
-   		        account_html_block += '<div class="col-sm-4">Last login</div><div class="col-sm-8">' + result['last_login'] + '</div>';
-                account_html_block += '</div>';
-                
-                $("#" + div_id).html(account_html_block);
-                updateStatus('Loaded account info.');
-            },
-            error: function (request, status, error) {
-                let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
-                updateStatus(html);
-            },
-            complete: function (data) {
+        
+        let options = {
+            method: 'GET',
+            headers: {
+              'Authorization': 'Bearer ' + api_key,
+              'Accept': 'application/vnd.heroku+json; version=3'
             }
-        });
+        };
+
+        fetch(heroku_api_url + 'account', options).then(function(response) {
+          return response.json();
+        }).then(function(data) {
+            let account_html_block = '';
+                
+            account_html_block += '<div class="row">';
+            account_html_block += '<div class="col-sm-4">ID</div><div class="col-sm-8">' + data['id'] + '</div>';
+            account_html_block += '<div class="col-sm-4">Name</div><div class="col-sm-8">' + data['name'] + '</div>';
+            account_html_block += '<div class="col-sm-4">Email</div><div class="col-sm-8">' + data['email'] + '</div>';
+            account_html_block += '<div class="col-sm-4">Sms number</div><div class="col-sm-8">' + data['sms_number'] + '</div>';
+            account_html_block += '<div class="col-sm-4">Identity Provider</div><div class="col-sm-8">' + data['identity_provider'] + '</div>';
+            account_html_block += '<div class="col-sm-4">Created at</div><div class="col-sm-8">' + data['created_at'] + '</div>';
+            account_html_block += '<div class="col-sm-4">Updated at</div><div class="col-sm-8">' + data['updated_at'] + '</div>';
+   		    account_html_block += '<div class="col-sm-4">Last login</div><div class="col-sm-8">' + data['last_login'] + '</div>';
+            account_html_block += '</div>';
+                
+            // Update account UI
+            $("#" + div_id).html(account_html_block);
+            // Update status bar
+            updateStatus('Loaded account info.');
+        }).catch(function(err) {
+            let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
+            updateStatus(html);
+        });        
     }else{
         let html = '<span style="color:red;">Invalid API key.</span>';
         updateStatus(html);
