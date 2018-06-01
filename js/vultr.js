ef11fa197a479a96ea5ced73d10489a5ef4cd8f4
yuxi <me@yuxifan.com>
2018-06-02 00:50:12 +0100

    refactoring: use fetch and Promises in update_vultr_snapshot_info

--- js/vultr.js
+++ js/vultr.js
@@ -128,7 +128,7 @@ const update_vultr_backup_info = (api_key, div_id) => {
               'API-Key':   api_key
             }
         };
-        fetch(vultr_api_url + 'backup/info', options).then(function(response) {
+        fetch(vultr_api_url + 'backup/list', options).then(function(response) {
             return response.json();
         }).then(function(result) {
             let backup_html_block = '';
@@ -157,34 +157,32 @@ const update_vultr_backup_info = (api_key, div_id) => {
 const update_vultr_snapshot_info = (api_key, div_id) => {
     if (api_key) {
         // Load Vultr snapshot information
-        $.ajax({
-            type: "GET",
-            url: vultr_api_url+'snapshot/list',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('API-Key', api_key);
-            },
-            success: function (result) {
-                let snapshot_html_block = '';
-                for(var s in result) {
-					snapshot_html_block += '<div class="row divblock">';
-					snapshot_html_block += '<div class="col-sm-4">Description</div><div class="col-sm-8">' + result[s]['description'] + '</div>';
-                    snapshot_html_block += '<div class="col-sm-4">ID</div><div class="col-sm-8">' + result[s]['SNAPSHOTID'] + '</div>';
-					snapshot_html_block += '<div class="col-sm-4">Date</div><div class="col-sm-8">' + result[s]['date_created'] + '</div>';
-					snapshot_html_block += '<div class="col-sm-4">Size</div><div class="col-sm-8">' + parseFloat(result[s]['size'])/(1024*1024*1024) + ' GB' + '</div>';
-                    snapshot_html_block += '<div class="col-sm-4">Status</div><div class="col-sm-8">' + result[s]['status'] + '</div>';
-                    snapshot_html_block += '</div>';
-                }
-
-                $("#" + div_id).html(snapshot_html_block);
-                updateStatus('Loaded snapshot information.');
-            },
-            error: function (request, status, error) {
-                let html = '<span style="color:red;">Error: ' + request.responseText + '</span>';
-                updateStatus(html);
-            },
-            complete: function (data) {
+        let options = {
+            method: 'GET',
+            headers: {
+              'API-Key':   api_key
             }
-        });
+        };
+        fetch(vultr_api_url + 'snapshot/list', options).then(function(response) {
+            return response.json();
+        }).then(function(result) {
+            let snapshot_html_block = '';
+            for(var s in result) {
+                snapshot_html_block += '<div class="row divblock">';
+                snapshot_html_block += '<div class="col-sm-4">Description</div><div class="col-sm-8">' + result[s]['description'] + '</div>';
+                snapshot_html_block += '<div class="col-sm-4">ID</div><div class="col-sm-8">' + result[s]['SNAPSHOTID'] + '</div>';
+                snapshot_html_block += '<div class="col-sm-4">Date</div><div class="col-sm-8">' + result[s]['date_created'] + '</div>';
+                snapshot_html_block += '<div class="col-sm-4">Size</div><div class="col-sm-8">' + parseFloat(result[s]['size'])/(1024*1024*1024) + ' GB' + '</div>';
+                snapshot_html_block += '<div class="col-sm-4">Status</div><div class="col-sm-8">' + result[s]['status'] + '</div>';
+                snapshot_html_block += '</div>';
+            }
+
+            $("#" + div_id).html(snapshot_html_block);
+            updateStatus('Loaded snapshot information.');
+        }).catch(function(err) {
+            let html = '<span style="color:red;">Error: ' + err + '</span>';
+            updateStatus(html);
+        });  
     }else{
         let html = '<span style="color:red;">Invalid API key.</span>';
         updateStatus(html);
